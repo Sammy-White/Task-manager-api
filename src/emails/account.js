516b052f07a351154584e3db60f330cf24f5cfc1
Samson Ugwu <samsonugwu802@gmail.com>
2020-04-26 17:14:13 -1200

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,57 @@
+const sgMail = require('@sendgrid/mail')
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const msg = {
+    to: 'samson.ugwu.com@gmail.com',
+    from: 'samson.ugwu.com@gmail.com',
+    subject: 'This is my first creation!',
+    text: 'I hope this one actually get to you'
+}
+
+const sendWelcomeEmail = (email, name) =>{
+    sgMail.send({
+        to: email, 
+        from: 'samson.ugwu.com@gmail.com', 
+        subject: 'Registration Successful', 
+        text: `Dear ${name}, Your registration to the Task Manager was successful. Try exploring interesting features of the app`
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email, 
+        from: 'samson.ugwu.com@gmail.com', 
+        subject: 'Deleting Your Account', 
+        text: `Dear ${name}, your task manager app account has been deleted, we would like to know if there any thing we can do to keep you back`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// .then(() => console.log('Mail sent successfully'))
+// .catch(error => console.error(error.toString()));
+
+
+
